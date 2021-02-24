d6179316f37ee36fdb8179059dc128d0dbaf754d
camillecalas@hotmail.fr <camillecalas@hotmail.fr>
2021-02-24 18:02:25 +0100

    finitions

---
+++ main.js
@@ -0,0 +1,15 @@
+const inputs = document.querySelectorAll ('input');
+
+inputs.forEach(input => {
+    input.addEventListener('click', (e) =>{
+        inputs.forEach(input =>{
+            if(input.checked){
+                input.checked = false;
+                e.target.checked = true;
+            }
+        })
+    })
+});
+
+
+
