e59d79ab8d5a9b6403f2022888e408a617447a80
camillecalas@hotmail.fr <camillecalas@hotmail.fr>
2021-02-25 09:31:31 +0100

    finition

--- main.js
+++ main.js
@@ -11,21 +11,5 @@ inputs.forEach(input => {
     })
 });
 
-let observer = new IntersectionObserver(function(observables){
-    observables.forEach(function(observable){
-        if(observable.intersectionRatio > 0.5){
-            observable.target.classList.remove('not-visble')
-        }
-    })
-}, {
-threshold: [0.5]
-
-})
-
-let items = document.querySelectorAll('.text')
-items.forEach(function(item){
-    item.classList.add('not-visible')
-    observer.observe(item)
-})
 
 
