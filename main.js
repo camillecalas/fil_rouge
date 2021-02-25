68524321e02ea2585e2a879b8b8e1e972e9fbaea
camillecalas@hotmail.fr <camillecalas@hotmail.fr>
2021-02-25 15:12:05 +0100

    finitions

--- main.js
+++ main.js
@@ -31,6 +31,18 @@ inputs.forEach(input => {
 
 
 
-new fullpage('#fullpage', {
-    autoScrolling: true,
-})
+
+const elements = document.querySelectorAll('.anim');
+const observer = new IntersectionObserver ((entries) => {
+    entries.forEach((entry)=>{
+        if(entry.intersectionRatio > 0){
+            entry.target.classList.add('anim-reveal');
+        } else {
+            entry.target.classList.remove('anim-reveal');
+        }
+    })
+});
+
+    elements.forEach(element =>{
+        observer.observe(element)
+    });
