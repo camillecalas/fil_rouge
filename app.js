7fa1bad6a07a5d10d2532bd2a54db3f95496128e
camillecalas@hotmail.fr <camillecalas@hotmail.fr>
2021-02-25 08:31:10 +0100

    finitions

--- app.js
+++ app.js
@@ -1,16 +1,3 @@
-const inputs = document.querySelectorAll ('input');
-
-inputs.forEach(input => {
-    input.addEventListener('click', (e) =>{
-        inputs.forEach(input =>{
-            if(input.checked){
-                input.checked = false;
-                e.target.checked = true;
-            }
-        })
-    })
-});
-
 let observer = new IntersectionObserver(function(observables){
     observables.forEach(function(observable){
         if(observable.intersectionRatio > 0.5){
@@ -27,5 +14,3 @@ items.forEach(function(item){
     item.classList.add('not-visible')
     observer.observe(item)
 })
-
-
